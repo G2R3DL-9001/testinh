80ad296b832d9b7cbf8daa5643e5db1761782a6d
G2R3DL-9001 <67333255+G2R3DL-9001@users.noreply.github.com>
2026-02-13 18:51:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Merhaba, Coolify çalışıyor!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
